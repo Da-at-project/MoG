21898206a7fcf37cfef651bc6bc651128cbeadc5
이준원 <ljw0121l@gmail.com>
2023-10-18 17:25:53 +0900

    적 Ui, 허수아비 피격틀
    Enemy HpBar, Enemy HpSystem, Dummy Damaged

---
+++ Assets/Caracter/dumy/RatHp.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RatHp : MonoBehaviour
+{
+    public Slider mySlider;
+    public float maxHp;
+    public float curHp;
+
+    private GameObject HpBar;
+    void Start()
+    {
+        HpBar = GameObject.Find("RatCanvas/HpVar");
+    }   
+
+    private void LateUpdate()
+    {
+        mySlider.value = curHp / maxHp;
+    }
+    private void Update()
+    {
+        HpBar.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, -0.8f, 0));
+    }
+}
